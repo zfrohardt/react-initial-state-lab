811737963fe932688a55e944db4ac6ac8c9debbc
Zach Frohardt <zfrohardt@gmail.com>
2021-04-26 11:58:13 -0700

    completed assignment

--- src/Bomb.js
+++ src/Bomb.js
@@ -1,1 +1,14 @@
 // your Bomb code here!
+import React, {Component} from 'react';
+
+export default class Bomb extends Component {
+    constructor(props) {
+        super();
+        this.state = {
+            secondsLeft: props.initialCount,
+        }
+    }
+    render() {
+        return (this.state.secondsLeft === 0)? 'Boom!' : `${this.state.secondsLeft} seconds left before I go boom!`;
+    }
+}
--- src/ImageSlider.js
+++ src/ImageSlider.js
@@ -1,1 +1,14 @@
 // your ImageSlider code here!
+import React, {Component} from 'react';
+
+export default class ImageSlider extends Component {
+    constructor() {
+        super();
+        this.state = {
+            currentSlideIndex: 0,
+        }
+    }
+    render() {
+        return `I am on slide ${this.state.currentSlideIndex}`;
+    }
+}
